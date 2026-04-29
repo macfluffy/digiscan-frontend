82e124dfb9fa192ccf5c4a3d7b42e7d023eded80
macfluffy <shadowimitation@hotmail.com>
2026-04-30 02:13:32 +1000

    init: Starting website using vue framework

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Footer from './shared/components/footer/footer.vue';
+// import HelloWorld from './shared/components/HelloWorld.vue'
+import SearchBar from './shared/components/searchBar/searchBar.vue'
+</script>
+
+<template>
+  <!-- <HelloWorld /> -->
+  <SearchBar />
+  <Footer />
+</template>
