d824f5a83e5df1cfd8ff54921833ea834fc29c7d
macfluffy <shadowimitation@hotmail.com>
2026-04-30 02:14:24 +1000

    feat: Adding the footer component first with its own styling

---
+++ src/shared/components/footer/footer.vue
@@ -0,0 +1,41 @@
+<template>
+    <footer class="flex-container flex-rows">
+
+        <section class= "footer flex-container flex-columns" id="cards-footer">
+            <!-- <div class="footer-column"> -->
+                <h6>Cards</h6>
+                <ul class="footer-links">
+                    <li>Advanced Search</li>
+                    <li>All Sets</li>
+                    <li>Random Card</li>
+                </ul>
+            <!-- </div> -->
+        </section>
+
+        <section class="footer flex-container flex-columns" id="account-footer">
+            <!-- <div class="footer-column"> -->
+                <h6>Account</h6>
+                <ul class="footer-links">
+                    <li>Your Account</li>
+                    <li>Register</li>
+                    <li>Your Decks</li>
+                    <li>Your Match History</li>
+                </ul>
+            <!-- </div> -->
+        </section>
+
+        <section class="footer flex-container flex-columns" id="digiscan-footer">
+            <!-- <div class="footer-column"> -->
+                <h6>Digiscan</h6>
+                <ul class="footer-links">
+                    <li>Terms of Service</li>
+                    <li>Privacy Policy</li>
+                    <li>API Documentation</li>
+                </ul>
+            <!-- </div> -->
+        </section>
+
+    </footer>
+</template>
+
+<style scoped src="./style.css" />
