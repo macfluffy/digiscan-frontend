7a334673351bd4dffdd58762fe5c5219ebcac59e
macfluffy <shadowimitation@hotmail.com>
2026-04-30 12:58:36 +1000

    feat! : Changed style.css to footer.css. Changing the naming convention to give distinction to what the css styling is for.

--- src/shared/components/footer/footer.vue
+++ src/shared/components/footer/footer.vue
@@ -2,40 +2,34 @@
     <footer class="flex-container flex-rows">
 
         <section class= "footer flex-container flex-columns" id="cards-footer">
-            <!-- <div class="footer-column"> -->
-                <h6>Cards</h6>
-                <ul class="footer-links">
-                    <li>Advanced Search</li>
-                    <li>All Sets</li>
-                    <li>Random Card</li>
-                </ul>
-            <!-- </div> -->
+            <h6>Cards</h6>
+            <ul class="footer-links">
+                <li>Advanced Search</li>
+                <li>All Sets</li>
+                <li>Random Card</li>
+            </ul>
         </section>
 
         <section class="footer flex-container flex-columns" id="account-footer">
-            <!-- <div class="footer-column"> -->
-                <h6>Account</h6>
-                <ul class="footer-links">
-                    <li>Your Account</li>
-                    <li>Register</li>
-                    <li>Your Decks</li>
-                    <li>Your Match History</li>
-                </ul>
-            <!-- </div> -->
+            <h6>Account</h6>
+            <ul class="footer-links">
+                <li>Your Account</li>
+                <li>Register</li>
+                <li>Your Decks</li>
+                <li>Your Match History</li>
+            </ul>
         </section>
 
         <section class="footer flex-container flex-columns" id="digiscan-footer">
-            <!-- <div class="footer-column"> -->
-                <h6>Digiscan</h6>
-                <ul class="footer-links">
-                    <li>Terms of Service</li>
-                    <li>Privacy Policy</li>
-                    <li>API Documentation</li>
-                </ul>
-            <!-- </div> -->
+            <h6>Digiscan</h6>
+            <ul class="footer-links">
+                <li>Terms of Service</li>
+                <li>Privacy Policy</li>
+                <li>API Documentation</li>
+            </ul>
         </section>
 
     </footer>
 </template>
 
-<style scoped src="./style.css" />
+<style scoped src="./footer.css" />
