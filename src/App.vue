2a65f1c07a8bebdb98d39a6ed16fc5d334a6830a
macfluffy <shadowimitation@hotmail.com>
2026-06-29 18:35:00 +1000

    feat: Attached the router to the vue application

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-  import FrontPage from './pages/frontPage/frontPage.vue';
   import Footer from './shared/components/footer/footer.vue';
   import SearchBar from './shared/components/searchBar/searchBar.vue'
 </script>
 
 <template>
   <SearchBar />
-  <FrontPage />
+  <RouterView />
   <Footer />
 </template>
