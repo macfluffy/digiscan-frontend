3e6fa78fe3c39351766be38a0f0ead5ad42b5077
macfluffy <shadowimitation@hotmail.com>
2026-07-08 15:29:55 +1000

    feat: Added the Advanced Search Page to the client router

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import FrontPage from "../pages/frontPage/frontPage.vue";
 import AdvancedSearch from "../pages/advancedSearch/advancedSearch.vue";
 import Sets from "../pages/sets/sets.vue";
+import SearchResults from "../pages/searchResults/searchResults.vue";
 
 const routes = [
     { path: "/", component: FrontPage },
     { path: "/advancedSearch", component: AdvancedSearch },
+    { path: "/search", component: SearchResults },
     { path: "/sets", component: Sets },
 ];
 
