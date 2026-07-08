e5dcf9573fe63ddde8a43bc80be0d1d5e3b795c3
macfluffy <shadowimitation@hotmail.com>
2026-07-08 15:28:09 +1000

    feat: The search bar component now pushes the search results page whenever a user submits a search query. The query will appear in the url which will be handled by the search results page, resulting in not needing states to be handled.

--- src/shared/components/searchBar/searchBar.vue
+++ src/shared/components/searchBar/searchBar.vue
@@ -1,7 +1,29 @@
+<script setup>
+    import { ref } from 'vue';
+    import { useRouter } from 'vue-router';
+    
+    const router = useRouter();
+    const query = ref('');
+
+    function submitSearch() {
+        const searchQuery = query.value.trim;
+        router.push({
+            path: "/search",
+            query: {
+                // Return a null if the searchQuery is empty
+                cardName: searchQuery || null
+            }
+        });
+    }   
+</script>
+
 <template>
-    <input 
-        type = "text" 
-        placeholder = "Enter card name here" 
-        class = "search-bar"
-    >
+    <form>
+        <input 
+            type = "text" 
+            placeholder = "Enter card name here" 
+            class = "search-bar"
+            @submit="submitSearch"
+        >
+    </form>
 </template>
