096ce6243fef36897a0c968e767c0ba3e73648e7
macfluffy <shadowimitation@hotmail.com>
2026-07-20 11:46:20 +1000

    feat: Added path to the Card component. This will take the user to the card's information page, usually when selected from the search results page, but also from shared links.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import FrontPage from "../pages/frontPage/frontPage.vue";
 import AdvancedSearch from "../pages/advancedSearch/advancedSearch.vue";
-import Sets from "../pages/sets/sets.vue";
+import Card from "../pages/card/card.vue";
+import FrontPage from "../pages/frontPage/frontPage.vue";
 import SearchResults from "../pages/searchResults/searchResults.vue";
+import Sets from "../pages/sets/sets.vue";
 
 const routes = [
     { path: "/", component: FrontPage },
     { path: "/advancedSearch", component: AdvancedSearch },
+    { path: "/card/:cardSet/:cardNumber/:cardName", component: Card },
     { path: "/search", component: SearchResults },
     { path: "/sets", component: Sets },
 ];
