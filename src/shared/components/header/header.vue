ddd3d2d101543eba6e54c6b3442c785fadf05996
macfluffy <shadowimitation@hotmail.com>
2026-08-05 20:52:21 +1000

    feat: Added the header component, placing the logo and the search bar inside the header

---
+++ src/shared/components/header/header.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import { useRouter } from 'vue-router';
+
+    import SearchBar from '../searchBar/searchBar.vue';
+
+    import logo from '../../../assets/digiscan_header_logo_black_white.png';
+
+    const router = useRouter();
+</script>
+
+<template>
+    <div class="flex-container" id="search-bar-container">
+        <RouterLink to="/">
+            <img :src="logo" 
+                alt="Header Icon"
+                id="header-logo" 
+            />
+        </RouterLink>
+
+        <SearchBar id="header-search-bar" />
+    </div>
+</template>
+
+<style scoped src="./header.css" />
