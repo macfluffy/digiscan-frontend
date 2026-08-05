2568881fcc4c21a13b27a23c38281e3c0b58f7a8
macfluffy <shadowimitation@hotmail.com>
2026-08-05 20:52:48 +1000

    feat! : Changed the additional SearchBar component in the app to the header.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
+  import Header from './shared/components/header/header.vue';
   import Footer from './shared/components/footer/footer.vue';
-  import SearchBar from './shared/components/searchBar/searchBar.vue'
+  // import SearchBar from './shared/components/searchBar/searchBar.vue'
 </script>
 
 <template>
-  <SearchBar />
+  <Header />
   <RouterView />
   <Footer />
 </template>
