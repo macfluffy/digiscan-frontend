9c7ef2aa8ec11ae9d7c02cff74e2f5320b464b87
macfluffy <shadowimitation@hotmail.com>
2026-09-14 19:33:50 +1000

    fix: Clicking on the table row will now take the user to the card gallery of that set, instead of only the set number.

--- src/pages/sets/sets.vue
+++ src/pages/sets/sets.vue
@@ -1,10 +1,17 @@
 <script setup>
     import { ref, onMounted } from "vue";
     import { getAllSets } from "../../api/cardSetServices.js";
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
 
     const sets = ref([]);
     const loading = ref(false);
     const error = ref(null);
+
+    function gotoSet(setNumber) {
+        router.push(`/set/${setNumber}`);
+    }
 
     onMounted(async () => {
         loading.value = true;
@@ -31,13 +38,11 @@
             </tr>
         </thead>
         <tbody>
-            <tr class="hoverable-row" v-for="set in sets" :key="set.id">
-                <td>
-                    <RouterLink :to="{ path: `/set/${set.setNumber}` }"
-                        class="set-number">
-                        {{ set.setNumber }}
-                    </RouterLink>
-                </td>
+            <tr class="hoverable-row" 
+                v-for="set in sets" 
+                :key="set.id" 
+                @click="gotoSet(set.setNumber)">
+                <td class="set-number">{{ set.setNumber }}</td>
                 <td>{{ set.setName }}</td>
                 <td class="block-number">{{ set.blockNumber }}</td>
                 <td class="print-date">{{ set.printDate }}</td>
